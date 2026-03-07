53a4d058e870584bb107f02ed124768673b6ea9c
KeeranNaidu12 <keerannaidu08@gmail.com>
2026-03-07 12:29:57 -0500

    Working on improving Landing Page

--- my-app/src/pages/Landing.tsx
+++ my-app/src/pages/Landing.tsx
@@ -4,20 +4,28 @@ function Landing() {
     const navigate = useNavigate()
 
     return (
-        <main>
+        <main className='min-h-screen bg-gray-300'>
+            <div>
             <section>
-                <p className="landing-kicker m-10 font-bold text-5xl">Hackathon 2026</p>
-                <h1 className='m-10 text-2xl'>Validate business decisions before you commit.</h1>
-                <p className='m-10 text-2xl'>
+                <h1 className="landing-kicker m-10 p-3 bg-blue-600 rounded-xl text-white font-bold text-5xl flex justify-center">Hackathon 2026</h1>
+                <p className='m-10 text-2xl flex justify-center'>Validate business decisions before you commit.</p>
+                
+                <div className="bg-blue-100 rounded-lg p-3 m-10">
+                <p className='m-10 text-xl flex justify-center'>
                     Unsure about making changes to your business? Run simulations to see whether your
                     idea is likely to help growth, hurt margins, or create hidden risk.
                 </p>
-                <div className="landing-actions">
-                    <button type="button" className='m-10 p-2 bg-blue-500 rounded-lg text-white' onClick={() => navigate('/simulation')}>
+                </div>
+
+
+                <div className="landing-actions flex justify-center">
+                    <button type="button" className='m-10 p-2 bg-blue-500 rounded-lg text-white text-xl text-center' onClick={() => navigate('/simulation')}>
                         Start your simulation
                     </button>
                 </div>
             </section>
+
+            </div>
         </main>
     )
 }
